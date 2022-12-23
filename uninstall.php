07ad5f88272d10073b40721f64022043fe0e5b56
sgagolaz <veronelli.davide@gmail.com>
2022-12-23 15:54:31 +0100

    Corretto uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,6 @@
 <?php
 // check that the script was not directly called
-if (isset(WP_UNINSTALL_PLUGIN)) {
-	// remove the only option of the plugin
-	delete_option(WorkWithUsPlugin::HTML_OPTION_NAME);
+if (defined('WP_UNINSTALL_PLUGIN')) {
+	// remove the option of the plugin
+	delete_option('wwup_call_to_action_option');
 }
