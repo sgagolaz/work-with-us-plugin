48664b62af5973f1d43cc5c65719308f75a5b984
sgagolaz <veronelli.davide@gmail.com>
2022-12-23 15:55:35 +0100

    Corretto uninstall
    Aggiornato numero versione

--- work-with-us-plugin.php
+++ work-with-us-plugin.php
@@ -6,7 +6,7 @@
 Plugin Name: Work With Us ilpost.it
 Plugin URI: https://github.com/sgagolaz/work-with-us-plugin
 Description: Job application test. This plugin inserts a call-to-action after the 4th paragraph of posts tagged as "governo".
-Version: 1.0.0
+Version: 1.0.1
 Requires at least: 6.1.1
 Requires PHP: 8.1.12
 Author: sgagolaz
